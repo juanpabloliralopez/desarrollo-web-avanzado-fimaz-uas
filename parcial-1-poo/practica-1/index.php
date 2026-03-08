b41615a25cf68772f5d08320513c442b4d561f71
JuanLira <juanpabl0lira1717@gmail.comgit config --global user.email juanpabl0lira1717@gmail.comgit config --global user.email juanpabl0lira1717@gmail.com>
2026-03-07 17:34:37 -0700

    archivo index comentada

--- parcial-1-poo/practica-1/index.php
+++ parcial-1-poo/practica-1/index.php
@@ -1,19 +1,33 @@
- <?php
+<?php
+//archivo index - practica 1 - lira lopez //
 
- require_once 'Usuario.php';
+// Aqui inclui el archivo de la clase Usuario para que hablen el mismo idioma
+require_once 'Usuario.php';
 
- $usuario = new Usuario("Juan Pablo Lira Lopez", "juanpabl0lira1717@gmail.com");
- ?>
+// Cree una instancia de la clase usuario y de ahi se envian los datos al constructor que serian el nombre y el correo
+$usuario = new Usuario("Juan Pablo Lira Lopez", "juanpabl0lira1717@gmail.com");
+?>
 
- <!DOCTYPE html>
+<!DOCTYPE html>
+
 <html lang="es">
+
 <head>
+
     <meta charset="UTF-8">
+
     <title>Práctica 1 - Lira</title>
+
 </head>
+
 <body>
+
     <h1>Información del Usuario</h1>
+
     <p><strong>Nombre:</strong> <?php echo $usuario->getNombre(); ?></p>
+
     <p><strong>Correo:</strong> <?php echo $usuario->getCorreo(); ?></p>
+
 </body>
+
 </html>
